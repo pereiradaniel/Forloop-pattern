bc7ec86fd9c5290f3114c30ad3b77cee0eecd93d
pereiradaniel <3382120-pereiradaniel@users.noreply.replit.com>
2023-12-28 22:22:39 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void) {
+  // Array of strings that form the pattern:
+  char *a[] = {
+    "********* ",
+    "*   *   * ",
+    "    *     ",
+    "    *     ",
+    "    ******",
+    "    *    *",
+    "    *    *",
+    "    *    *",
+  };
+
+  // Find array length for use in loop:
+  int aLength = sizeof(a) / sizeof(a[0]);
+
+  // Loop and print each array element to form patter:
+  for (int i = 0; i < aLength; ++i)
+    printf("%s\n", a[i]);
+
+  return 0;
+}
+
+// 28 Dec 2023
