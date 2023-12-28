9759e8c522a97beaf312559f2f794408198def2b
pereiradaniel <13129650+pereiradaniel@users.noreply.github.com>
2023-12-28 22:39:21 +0000

    Correct typo.

--- main.c
+++ main.c
@@ -16,11 +16,10 @@ int main(void) {
   // Find array length for use in loop:
   int aLength = sizeof(a) / sizeof(a[0]);
 
-  // Loop and print each array element to form patter:
+  // Loop and print each array element to form pattern:
   for (int i = 0; i < aLength; ++i)
     printf("%s\n", a[i]);
 
   return 0;
 }
-
 // 28 Dec 2023
